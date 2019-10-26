836c4c66413e78003ad1e1f095cc060897fe1be5
abdullahfurat <a_furat@hotmail.com>
2019-10-26 14:14:09 +0300

    son hal

--- Program.cs
+++ Program.cs
@@ -10,6 +10,76 @@ namespace Degiskenler
     {
         static void Main(string[] args)
         {
+
+            /* BYTE
+                     Summary:
+                     Represents the largest possible value of a System.Byte. This field is constant.
+                           MaxValue = 255;
+                           MinValue = 0;*/
+            byte s1 = 255;
+
+
+            /* SBYTE
+                 Summary:
+                 Represents the largest possible value of a System.Byte. This field is constant.
+                         MaxValue = 127;
+                         MinValue = -128;*/
+
+            sbyte s2 = -128;
+
+            /* SHORT
+           Summary:
+           Represents the largest possible value of a System.Byte. This field is constant.
+                   MaxValue =  32767;
+                   MinValue = -32768;*/
+
+            short s3 = 32767;
+
+            /* USHORT
+             Summary:
+             Represents the largest possible value of a System.Byte. This field is constant.
+                    MaxValue =  65535;
+                    MinValue = 0;*/
+            ushort s4 = 65535;
+
+            /* int
+                 Summary:
+                 Represents the largest possible value of a System.Byte. This field is constant.
+                       MaxValue =  2147483647;
+                       MinValue = -2147483648;*/
+
+            int s5 = 2147483647;
+
+            /* uint
+                    Summary:
+                     Represents the largest possible value of a System.Byte. This field is constant.
+                      MaxValue =  4294967295;
+                      MinValue = 0;*/
+            uint s6 = 4294967295;
+
+            /* long
+                 Summary:
+                 Represents the largest possible value of a System.Byte. This field is constant.
+                      MaxValue =  9223372036854775807;
+                      MinValue = -9223372036854775808;*/
+            long s7 = 9223372036854775807;
+            /* ulong
+             Summary:
+             Represents the largest possible value of a System.Byte. This field is constant.
+                  MaxValue =  18446744073709551615;
+                  MinValue = 0;*/
+            ulong s8 = 18446744073709551615;
+
+
+
+
+            /* double
+           Summary:
+           Represents the largest possible value of a System.Byte. This field is constant.
+                MaxValue =  1.7976931348623157E+308;
+                MinValue = -1.7976931348623157E+308;*/
+            double s9 = 1.7976931348623157E+308;
+
         }
     }
 }
